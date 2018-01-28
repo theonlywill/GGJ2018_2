6199954c358f9418deef1a96b3cd44371e76b971
Will Teixeira <willrteixeira@gmail.com>
2018-01-28 10:05:52 -0500

    dialog window tech
    weeeeeeeeeeeeeeeeeeeeeeee!!!!!!!!!!!!!!

---
+++ source/GGJ2018_src/Assets/Scripts/UI/DialogWindow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogWindow : MonoBehaviour {
+
+    public GameObject clickBlocker;
+    public GameObject canvas;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnClicked()
+    {
+        clickBlocker.SetActive(false);
+        canvas.SetActive(false);
+    }
+}
