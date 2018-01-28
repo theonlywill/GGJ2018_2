89240aebd5e07f54f4d9fbc9a5d1795c2b98dbe2
MSI\Kristy <kristymoniz731@gmail.com>
2018-01-28 10:55:32 -0500

    Revert "Merge branch 'master' of https://github.com/theonlywill/GGJ2018_2"
    This reverts commit f451359b2650c13103bdd8c546e2c67b23254394, reversing
    changes made to be10e747ada768328e07866187ff3205a51eee90.

--- source/GGJ2018_src/Assets/Scripts/UI/DialogWindow.cs
+++ source/GGJ2018_src/Assets/Scripts/UI/DialogWindow.cs
@@ -7,15 +7,6 @@ public class DialogWindow : MonoBehaviour {
     public GameObject clickBlocker;
     public GameObject canvas;
 
-    public List<Dialog> dialogs = new List<Dialog>();
-
-    [System.Serializable]
-    public class Dialog
-    {
-        public GameObject window;
-        public AudioClip audioClip;
-    }
-
 	// Use this for initialization
 	void Start () {
 		
@@ -28,27 +19,7 @@ public class DialogWindow : MonoBehaviour {
 
     public void OnClicked()
     {
-        bool closedDialog = false;
-        for(int i = 0; i < dialogs.Count; i++)
-        {
-            if(dialogs[i].window.activeSelf)
-            {
-                dialogs[i].window.SetActive(false);
-                closedDialog = true;
-                AudioSource audioSource = GetComponent<AudioSource>();
-                if(audioSource && dialogs[i].audioClip)
-                {
-                    audioSource.PlayOneShot(dialogs[i].audioClip);
-                }
-
-                break;
-            }
-        }
-
-        if (!closedDialog)
-        {
-            // we closed our last dialog
-            canvas.SetActive(false);
-        }
+        clickBlocker.SetActive(false);
+        canvas.SetActive(false);
     }
 }
